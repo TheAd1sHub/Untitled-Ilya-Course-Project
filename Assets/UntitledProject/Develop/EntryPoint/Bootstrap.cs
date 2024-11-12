73263a4973e0921b0de0b87e546854615cdd5f34
Ad1s <y.bryak@gmail.com>
2024-11-12 23:37:10 +0200

    Set up the base for the "Bootstrap" scene

---
+++ Assets/UntitledProject/Develop/EntryPoint/Bootstrap.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using Assets.UntitledProject.Develop.DI;
+using System.Collections;
+
+namespace Assets.UntitledProject.Develop.EntryPoint
+{
+	public sealed class Bootstrap : MonoBehaviour
+	{
+		public IEnumerator Run(DIContainer container)
+		{
+			// TODO: Start a Loading Animation
+
+			// TODO: Initialize All the Data, Configs, Services, etc.
+
+			yield return null;
+
+			// TODO: Finish the Loading Animation
+
+			// TODO: Change Active Scene
+		}
+	}
+}
---
+++ Assets/UntitledProject/Develop/EntryPoint/EntryPoint.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using Assets.UntitledProject.Develop.DI;
+using System.Collections;
+
+namespace Assets.UntitledProject.Develop.EntryPoint
+{
+	public sealed class EntryPoint : MonoBehaviour
+	{
+		[SerializeField] private Bootstrap _gameBootstrap;
+
+		private void SetupAppSettings()
+		{
+			QualitySettings.vSyncCount = 0;
+
+#if UNITY_EDITOR
+			Application.targetFrameRate = 144;
+#else
+			Application.targetFrameRate = 60;
+#endif
+		}
+
+		private void Awake()
+		{
+			SetupAppSettings();
+
+			DIContainer projectContainer = new DIContainer();
+
+			// TODO: Register Global Services 
+
+			// TODO: Run Game Bootstrap with Global Project Container
+		}
+	}
+}
