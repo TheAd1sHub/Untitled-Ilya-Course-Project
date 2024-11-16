065e9b082d6aedb077f656fa0a38a968aa8c7d6a
Ad1s <y.bryak@gmail.com>
2024-11-17 01:20:06 +0200

    Add CoroutinesHandler service

---
+++ Assets/UntitledProject/Develop/CommonServices/CoroutinesHandler/CoroutinesHandler.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using UnityEngine;
+
+namespace Assets.UntitledProject.Develop.CommonServices.CoroutinesHandler
+{
+	public sealed class CoroutinesHandler : MonoBehaviour, ICoroutinesHandler
+	{
+		public Coroutine StartRoutine(IEnumerator coroutineFunction)
+			=> StartCoroutine(coroutineFunction);
+
+		public void StopRoutine(Coroutine coroutine)
+			=> StopCoroutine(coroutine);
+
+		private void Awake()
+		{
+			DontDestroyOnLoad(this);
+		}
+	}
+}
---
+++ Assets/UntitledProject/Develop/CommonServices/CoroutinesHandler/ICoroutinesHandler.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using UnityEngine;
+
+namespace Assets.UntitledProject.Develop.CommonServices.CoroutinesHandler
+{
+	public interface ICoroutinesHandler
+	{
+		public Coroutine StartRoutine(IEnumerator coroutineFunction);
+
+		public void StopRoutine(Coroutine coroutine);
+	}
+}
