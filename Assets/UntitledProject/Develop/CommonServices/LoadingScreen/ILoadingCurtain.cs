43434e4ae20e96f863c11810b1a4f8ca4fa1434f
Ad1s <y.bryak@gmail.com>
2024-11-17 01:21:17 +0200

    Add loading curtain logic & placeholder curtain

---
+++ Assets/UntitledProject/Develop/CommonServices/LoadingScreen/ILoadingCurtain.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+namespace Assets.UntitledProject.Develop.CommonServices.LoadingScreen
+{
+	public interface ILoadingCurtain
+	{
+		public bool IsShown { get; }
+
+		public void Show();
+		public void Hide();
+	}
+}
---
+++ Assets/UntitledProject/Develop/CommonServices/LoadingScreen/StandardLoadingCurtain.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+namespace Assets.UntitledProject.Develop.CommonServices.LoadingScreen
+{
+	public sealed class StandardLoadingCurtain : MonoBehaviour, ILoadingCurtain
+	{
+		public bool IsShown => gameObject.activeSelf;
+
+		public void Hide()
+		{
+			gameObject.SetActive(false);
+		}
+
+		public void Show()
+		{
+			gameObject.SetActive(true);
+		}
+
+		private void Awake()
+		{
+			Hide();
+			DontDestroyOnLoad(this);
+		}
+	}
+}
