a643ea186b8e83e45cfa298e9683a4a4410c1fd8
Ad1s <y.bryak@gmail.com>
2024-11-24 11:00:18 +0200

    Add ReactiveVariable
    Includes IReadOnlyVariable for safe accessibility

---
+++ Assets/UntitledProject/Develop/Utils/Reactive/IReadOnlyVariable.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Assets.UntitledProject.Develop.Utils.Reactive
+{
+	public interface IReadOnlyVariable<T>
+	{
+		public event Action<T, T> Changed;
+
+		T Value { get; }
+	}
+}
---
+++ Assets/UntitledProject/Develop/Utils/Reactive/ReactiveVariable.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Assets.UntitledProject.Develop.Utils.Reactive
+{
+	public class ReactiveVariable<T> : IReadOnlyVariable<T> where T : IEquatable<T> 
+	{
+		public event Action<T, T> Changed;
+
+		private T _value;
+
+		public ReactiveVariable(T value) => _value = value;
+
+		public ReactiveVariable() : this(default)
+		{
+		}
+
+		public T Value
+		{
+			get => _value;
+			set
+			{
+				T oldValue = _value;
+
+				_value = value;
+
+				if (_value.Equals(oldValue) == false)
+					Changed?.Invoke(oldValue, value);	
+			}
+		}
+    }
+}
