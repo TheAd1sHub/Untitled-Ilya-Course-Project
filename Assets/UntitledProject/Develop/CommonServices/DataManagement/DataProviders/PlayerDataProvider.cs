bac1f24c778c10b042a2c464978ce0459a9aaf64
Ad1s <y.bryak@gmail.com>
2024-11-24 11:47:07 +0200

    Add PlayerDataProvider
    Implemented a service handling PlayerData (for now - just player's funds)

---
+++ Assets/UntitledProject/Develop/CommonServices/DataManagement/DataProviders/PlayerDataProvider.cs
@@ -0,0 +1,32 @@
+﻿using Assets.UntitledProject.Develop.CommonServices.Wallet;
+using System;
+using System.Collections.Generic;
+
+namespace Assets.UntitledProject.Develop.CommonServices.DataManagement.DataProviders
+{
+	public class PlayerDataProvider : DataProvider<PlayerData>
+	{
+		public PlayerDataProvider(ISaveLoadService saveLoadService)
+			: base(saveLoadService)
+		{
+		}
+
+		protected override PlayerData GetOriginData()
+		{
+			return new PlayerData()
+			{
+				WalletData = GetInitialWalletData()
+			};
+		}
+
+		private Dictionary<CurrencyType, int> GetInitialWalletData()
+		{
+			Dictionary<CurrencyType, int> walletData = new();
+
+			foreach (CurrencyType currencyType in Enum.GetValues(typeof(CurrencyType)))
+				walletData[currencyType] = 0;
+
+			return walletData;
+		}
+	}
+}
