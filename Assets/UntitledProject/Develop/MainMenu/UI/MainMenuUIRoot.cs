b264a519bb765e5fb742df21dc71ac1e1045f7fb
Ad1s <y.bryak@gmail.com>
2024-12-07 23:43:56 +0200

    Add currency & level selection UI View

---
+++ Assets/UntitledProject/Develop/MainMenu/UI/MainMenuUIRoot.cs
@@ -0,0 +1,14 @@
+﻿using Assets.UntitledProject.Develop.CommonUI;
+using UnityEngine;
+
+namespace Assets.UntitledProject.Develop.MainMenu.UI
+{
+	public sealed class MainMenuUIRoot : MonoBehaviour
+	{
+		[field: SerializeField] public IconsWithTextListView WalletView { get; private set; }
+
+		[field: SerializeField] public Transform HUDLayer { get; private set; }
+		[field: SerializeField] public Transform PopupsLayer { get; private set; }
+		[field: SerializeField] public Transform VFXLayer { get; private set; }	
+	}
+}
