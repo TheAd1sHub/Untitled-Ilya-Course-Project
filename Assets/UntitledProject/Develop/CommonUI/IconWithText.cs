9baf3c87e62a58b43f96c3e06ac4b9761a134504
Ad1s <y.bryak@gmail.com>
2024-12-07 22:57:40 +0200

    Add IconWithTextView & IconWithTextListView

---
+++ Assets/UntitledProject/Develop/CommonUI/IconWithText.cs
@@ -0,0 +1,16 @@
+﻿using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Assets.UntitledProject.Develop.CommonUI
+{
+	public sealed class IconWithText : MonoBehaviour
+	{
+		[SerializeField] private TMP_Text _text;
+		[SerializeField] private Image _icon;
+
+		public void SetText(string text) => _text.text = text;
+
+		public void SetIcon(Sprite sprite) => _icon.sprite = sprite;
+	}
+}
