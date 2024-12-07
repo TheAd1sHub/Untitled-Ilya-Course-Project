6ca301f66d2d60aeae777a7c9a8d6221cd2ff761
Ad1s <y.bryak@gmail.com>
2024-12-08 01:48:10 +0200

    Add ActionButton
    A simple script providing interaction interface between Button.onClick event and external code

---
+++ Assets/UntitledProject/Develop/CommonUI/ActionButton.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Assets.UntitledProject.Develop.CommonUI
+{
+	public sealed class ActionButton : MonoBehaviour
+	{
+		[SerializeField] private Button _button;
+
+		public void Initialize(Action onClickAction) => _button.onClick.AddListener(() => onClickAction?.Invoke());
+	}
+}
