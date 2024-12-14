8cfa91f82c6d2ea5a986cf14e239ce7723ff4eda
Ad1s <y.bryak@gmail.com>
2024-12-14 12:42:21 +0200

    Add LogicOperations shorthand class
    Just a class with static methods representing And and Or logic operations to reference while creating new CompositeConditions which use such simple operations as predicates.
    
    Example:
    ICompositeCondition condition = new CompositeCondition(LogicOperations.And).Add(/*FuncCondition logic*/);

---
+++ Assets/UntitledProject/Develop/Utils/Conditions/LogicOperations.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assets.UntitledProject.Develop.Utils.Conditions
+{
+	public static class LogicOperations
+	{
+		public static bool And(bool value1, bool value2) => value1 && value2;
+
+		public static bool Or(bool value1, bool value2) => value1 || value2;
+	}
+}
