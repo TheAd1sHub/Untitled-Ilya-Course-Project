b225d43c1318e1f2e19dfcc837268442105d98ff
Ad1s <y.bryak@gmail.com>
2024-12-14 12:46:36 +0200

    Add ReactiveEvent & ReactiveEvent<T>
    Implemented two classes for adding reactive logic to data-oriented gameplay handling.
    ReactiveEvent and ReactiveEvent<T> work with Action and Action<T> events respectively.

---
+++ Assets/UntitledProject/Develop/Utils/Reactive/ReactiveEvent.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Assets.UntitledProject.Develop.Utils.Reactive
+{
+	public sealed class ReactiveEvent
+	{
+		private List<ActionNode> _subscribers = new();
+
+		public IDisposable Subscribe(Action action)
+		{
+			ActionNode actionNode = new ActionNode(action, Remove);
+			_subscribers.Add(actionNode);
+			
+			return actionNode;
+		}
+
+		public void Invoke()
+		{
+			foreach (ActionNode subscriber in _subscribers)
+				subscriber.Invoke();	
+		}
+
+		private void Remove(ActionNode actionNode) => _subscribers.Remove(actionNode);
+
+		public sealed class ActionNode : IDisposable
+		{ 
+			private Action _action;
+			private Action<ActionNode> _onDispose;
+
+			public ActionNode(Action action, Action<ActionNode> onDispose)
+			{
+				_action = action;
+				_onDispose = onDispose;
+			}
+
+			public void Invoke() => _action?.Invoke();
+
+			public void Dispose() => _onDispose?.Invoke(this);
+		}
+	}
+
+	public sealed class ReactiveEvent<T> 
+	{
+		private List<ActionNode> _subscribers = new();
+
+		public IDisposable Subscribe(Action<T> action)
+		{
+			ActionNode actionNode = new ActionNode(action, Remove);
+			_subscribers.Add(actionNode);
+
+			return actionNode;
+		}
+
+		public void Invoke(T arg)
+		{
+			foreach (ActionNode subscriber in _subscribers)
+				subscriber.Invoke(arg);
+		}
+
+		private void Remove(ActionNode actionNode) => _subscribers.Remove(actionNode);
+
+		public sealed class ActionNode : IDisposable
+		{
+			private Action<T> _action;
+			private Action<ActionNode> _onDispose;
+
+			public ActionNode(Action<T> action, Action<ActionNode> onDispose)
+			{
+				_action = action;
+				_onDispose = onDispose;
+			}
+
+			public void Invoke(T arg) => _action?.Invoke(arg);
+
+			public void Dispose() => _onDispose?.Invoke(this);
+		}
+	}
+}
